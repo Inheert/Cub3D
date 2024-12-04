f1bb7a4a0160e93292bbbf709435afd72bfe2448
Inheert <theoclaereboudt@gmail.com>
2024-12-04 12:13:49 +0100

    gitignore

---
+++ Makefile
@@ -0,0 +1,46 @@
+NAME = cub3D
+
+CC = cc
+
+CFLAGS = -Wall -Wextra -Werror
+
+INCLUDES = -I ./includes/
+
+SRC_DIR = .
+BIN_DIR = ./bin
+
+SOURCES = main.c
+
+OBJECTS = $(addprefix $(BIN_DIR)/,$(SOURCES:.c=.o))
+
+MAKE_LIBFT = $(MAKE) -C ./src/utils/libft -s
+LIBFT = ./src/utils/libft/libft.a
+
+MAKE_GARBAGE = $(MAKE) -C ./src/utils/garbage_collector -s
+GARBAGE = ./src/utils/garbage_collector/garbage_collector.a
+
+all: $(NAME)
+
+$(NAME): $(OBJECTS)
+	@$(MAKE_GARBAGE)
+	@$(CC) $(OBJECTS) -o $@ $(INCLUDES) $(GARBAGE)
+	@echo "\033[1;38;5;10m   ⭐ - All files compiled.\033[0m"
+
+$(BIN_DIR)/%.o: $(SRC_DIR)/%.c
+	@mkdir -p $(dir $@)
+	@$(CC) $(CFLAGS) -c $< -o $@ $(INCLUDES)
+
+clean:
+	@rm -rf $(BIN_DIR)
+	@$(MAKE_GARBAGE) -s clean
+	@find . -name '*.o' -delete
+	@echo "\033[1;38;5;226m   📂 - bin folders removed.\033[0m"
+
+fclean: clean
+	@rm -f $(NAME)
+	@$(MAKE_GARBAGE) -s fclean
+	@echo "\033[1;38;5;9m   📤 - Libs and executable removed.\033[0m"
+
+re: fclean all
+
+.PHONY: all clean fclean re
---
+++ includes/cub3d.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tclaereb <tclaereb@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/04 10:25:47 by tclaereb          #+#    #+#             */
+/*   Updated: 2024/12/04 11:49:33 by tclaereb         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D_H
+# define CUB3D_H
+
+# include <stdio.h>
+
+# include "../src/utils/garbage_collector/includes/garbage_collector.h"
+
+#endif
